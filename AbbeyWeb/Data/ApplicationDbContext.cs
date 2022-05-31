9c110671ca3f1514dda6c2e5fba08cad25785c67
Sohel R Khan <sohel.khan7@hotmail.com>
2022-06-01 01:03:10 +0600

    3.9 DOne

---
+++ AbbeyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using AbbeyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbeyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ AbbeyWeb/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbeyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOreder { get; set; }
+    }
+}
